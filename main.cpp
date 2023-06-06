544c4ee4319c3383ee2c6f3e435728a21beeac89
rrivas <rrivas@utec.edu.pe>
2023-06-06 07:51:02 -0500

    Add sumatoria_paralela

--- main.cpp
+++ main.cpp
@@ -41,7 +41,7 @@ void sumar_rango_iteradores(int& resultado, Iterator start, Iterator stop) {
 }
 void sumatoria_vector_iteradores() {
     int nhilos = 4;
-    vector<int> v1 = {1, 7, 8, 20, 3, -5, 4, 2, 10, 11, 20, 21};
+    vector<int> v1 = {1, 7, 8, 20, 3, -5, 4, 2, 10, 11, 20, 21, 30};
     cout << "Total sin hilos: " << accumulate(begin(v1), end(v1), 0) << endl;
     // Resultados
     vector<int> resultado(nhilos);
@@ -80,8 +80,42 @@ void sumatoria_vector_iteradores() {
     cout << "Total: " << total << endl;
 }
 
+template <typename Iterator>
+auto sumatoria_paralela(Iterator start, Iterator stop) {
+    // Rangos de hilos y Datos
+    int nhilos = thread::hardware_concurrency();
+    int rango = distance(start, stop) / nhilos;
+    // Definimos Contenedores de Resultados y Hilos
+    using T = typename Iterator::value_type;
+    vector<T> resultado(nhilos);
+    vector<thread> vhilos(nhilos);
+
+    // Para asignar la tarea a cada hilo
+    for (int j = 0; j < vhilos.size(); ++j) {
+        // Calculo de Rangos usando iteradores
+        auto rstart = next(start,j*rango);
+        auto rstop = j < vhilos.size() - 1 ?
+                next( start, (j+1)*rango):
+                stop;
+        // 2. Asignarle la tarea o funcion
+        vhilos[j] = thread(
+                sumar_rango_iteradores<Iterator>,
+                ref(resultado[j]),
+                rstart,
+                rstop
+        );
+    }
+    // El hilo principal espera el resultado de los otros hilos}
+    for (int j = 0; j < vhilos.size(); ++j) {
+        vhilos[j].join();
+    }
+    return accumulate(begin(resultado), end(resultado), 0);
+}
+
 int main() {
 //    sumatoria_vector();
     sumatoria_vector_iteradores();
+    vector<int> v1 = {1, 7, 8, 20, 3, -5, 4, 2, 10, 11, 20, 21, 30};
+    cout << sumatoria_paralela(begin(v1), end(v1));
     return 0;
 }
